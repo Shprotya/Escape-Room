321478a30462c9b53d7ea9964bcde9320ca6aed6
Lisa <kareevaliza@gmail.com>
2025-11-20 20:53:32 +0000

    added a simple player class

---
+++ EscapeRoom/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeRoom
+{
+    class Player
+    {
+        // Player properties
+        public string Name { get; set; }
+        public List<string> Inventory { get; private set; }
+        public int Score { get; set; }
+        public int HintsUsed { get; set; }
+
+        // Constructor
+        public Player(string name)
+        {
+            Name = name;
+            Inventory = new List<string>();
+            Score = 1000; // Start with max score
+            HintsUsed = 0;
+        }
+    }
+}
